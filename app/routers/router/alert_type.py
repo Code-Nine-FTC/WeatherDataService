d8518c84fd8e5c8c936ae9612a04e9eaeac8dde4
DfMaciel <104574671+DfMaciel@users.noreply.github.com>
2025-03-16 03:19:19 -0300

    feat: #30 Rota de cadastro de tipos de alertas criada

---
+++ app/routers/router/alert_type.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.dependency.database import SessionConnection
+from app.modules.basic_response import BasicResponse
+from app.routers.controller.alert_type_controller import AlertTypeController
+from app.schemas.alert_type_schema import AlertTypeCreate, AlertTypeResponse
+
+router = APIRouter(tags=["Tipos de alerta"], prefix="/alert_type")
+
+
+# @router.get("/")
+# def hello_world() -> dict[str, str]:
+#     return {"msg": "Hello World"}
+
+
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_alert_type(
+    alert_type_data: AlertTypeCreate,
+    session: AsyncSession = Depends(SessionConnection),
+) -> BasicResponse[AlertTypeResponse]:
+    return await AlertTypeController(session).create_alert_type(alert_type_data)
