28883b670109fb32d96f49217af5e0f39a3740b2
DfMaciel <104574671+DfMaciel@users.noreply.github.com>
2025-03-16 03:17:03 -0300

    refactor: Nome do gerenciador de rotas modificado para melhor compreensão

---
+++ app/routers/define_routes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from fastapi import FastAPI
+
+from app.routers.router.alert_type import router as router_alert_type
+from app.routers.router.exemple import router
+
+
+def define_routes(app: FastAPI) -> None:
+    app.include_router(router)
+    app.include_router(router_alert_type)
