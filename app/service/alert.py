42f2d74dcaf2d3516323cf8a4ee40af6778a4f84
Yuri Braga de Lima <79583088+yuribragga@users.noreply.github.com>
2025-03-22 15:29:22 -0300

    feat: Criada a função de delet_alert clore issue #7
    Foi criada a função para deletar alertas.

--- app/service/alert.py
+++ app/service/alert.py
@@ -9,13 +9,14 @@ class AlertService:
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
 
-    async def delete_alert(self, id_alert: RequestAlert) -> None:  # Corrigido para aceitar um ID inteiro
-        result = await self._session.execute(select(Alert).where(Alert.id == id_alert))  # Usa o modelo Alert
+    async def delete_alert(self, id_alert: RequestAlert) -> None:
+        result = await self._session.execute(select(Alert).where(Alert.id == id_alert.id)) 
         alert = result.scalars().first()
         if alert is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Alerta com a ID {id_alert} não encontrado.",
+                detail=f"Alerta com a ID {id_alert.id} não encontrado.",
             )
-        await self._session.delete(alert)  # Corrigido para usar await
+        await self._session.delete(alert)
         await self._session.commit()
+
