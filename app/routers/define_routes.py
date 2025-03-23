ed37376ab8ae05845555046ed56acbb35ca9699a
Yuri Braga de Lima <79583088+yuribragga@users.noreply.github.com>
2025-03-23 12:20:08 -0300

    feat: Criado e colocado autenticação na rota de delete # 7

--- app/routers/define_routes.py
+++ app/routers/define_routes.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 from app.routers.router.alert_type import router as router_alert_type
 from app.routers.router.auth import router as router_auth
 from app.routers.router.weather_station import router as router_weather_station
+from app.routers.router.alert import router as router_alert
 
 
 def define_routes(app: FastAPI) -> None:
     app.include_router(router_weather_station)
     app.include_router(router_alert_type)
     app.include_router(router_auth)
+    app.include_router(router_alert)
