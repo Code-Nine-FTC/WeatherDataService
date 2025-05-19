3e579e97cc9d01a3e7313d0c4fc12326634d7c27
Elisa Rachel <elisarachel2@gmail.com>
2025-05-19 10:41:51 -0300

    fix: correção nos testes de carga

--- loadtest.js
+++ loadtest.js
@@ -1,13 +1,14 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { SharedArray } from 'k6/data';
 
 export let options = {
     vus: 200,
     duration: '1m',
 };
 
-export default function () {
-    // 1. LOGIN
+// Faz login 1x antes de começar o teste
+export function setup() {
     const loginPayload = {
         username: 'admcodenine@gmail.com',
         password: 'adm2025',
@@ -17,32 +18,35 @@ export default function () {
         'Content-Type': 'application/x-www-form-urlencoded',
     };
 
-    const loginRes = http.post('http://34.238.9.87:8000/auth/login', loginPayload, { headers: loginHeaders });
+    const res = http.post('http://34.238.9.87:8000/auth/login', loginPayload, { headers: loginHeaders });
 
-    check(loginRes, {
+    check(res, {
         'login status is 200': (r) => r.status === 200,
     });
 
-    const token = loginRes.json('access_token');
-    const authHeaders = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    };
+    const token = res.json('access_token');
+    return { token };
+}
 
-    // 2. ROTAS AUTENTICADAS
+export default function (data) {
     const endpoints = [
-        'http://34.238.9.87:8000/alert_type',
+        'http://34.238.9.87:8000/alert_type/',
         'http://34.238.9.87:8000/alert/all',
         'http://34.238.9.87:8000/alert_type/1',
-        'http://34.238.9.87:8000/dashboard/station-history',
-        'http://34.238.9.87:8000/parameter_type',
-        'http://34.238.9.87:8000/weather_station/1',
+        'http://34.238.9.87:8000/dashboard/station-history/1',
+        'http://34.238.9.87:8000/parameter_types/',
+        'http://34.238.9.87:8000/stations/1',
         'http://34.238.9.87:8000/dashboard/alert-types',
         'http://34.238.9.87:8000/dashboard/alert-counts',
         'http://34.238.9.87:8000/dashboard/station-status',
         'http://34.238.9.87:8000/dashboard/measures-status',
     ];
+
+    const authHeaders = {
+        headers: {
+            Authorization: `Bearer ${data.token}`,
+        },
+    };
 
     const res = http.get(endpoints[Math.floor(Math.random() * endpoints.length)], authHeaders);
 
